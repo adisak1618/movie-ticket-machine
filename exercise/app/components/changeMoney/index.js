bd94cab69d48e9d3fc4bbae240090a7de68ce31c
Adisak Chaiyakul <adiaskchaiyakul@gmail.com>
2019-07-11 16:41:17 +0700

    fix bug

--- exercise/app/components/changeMoney/index.js
+++ exercise/app/components/changeMoney/index.js
@@ -1,6 +1,6 @@
-export default ({ change = [] }) => <div>
+export default ({ change }) => <div>
   {
-    change.map((banknote, index) => {
+    (change || []).map((banknote, index) => {
       return <img key={`${banknote}-${index}`} className={banknote > 10 ? '': 'coin'} src={`/images/bank/${banknote}.${banknote > 10 ? 'jpg': 'png'}`} />;
     })
   }
